801e81d842820012231c7ee3fe13a71a83701569
tiutionmanager-max <harithaweerasekara128@gmail.com>
2025-12-28 08:31:08 +0530

    Json webtoken fixed by haritha

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,18 @@
-import jwt from "jsonwebtoken"
-export default function verifyJWT (req,res,next){
+import jwt from "jsonwebtoken";
 
-        const header = req.header("Authorization");
-        if(header != null){
+export default function verifyJWT(req, res, next) {
+  const header = req.header("Authorization");
 
-            const token = header.replace("Bearer ","")
-            jwt.verify(token,"random456",(err , decoded)=>{
-                console.log(decoded)
-                if(decoded != null){
+  if (header && header.startsWith("Bearer ")) {
+    const token = header.replace("Bearer ", "");
 
-                    req.user = decoded
-                    
-                }
-            })
-        }
-        
-        next()
-
+    jwt.verify(token, "random456", (err, decoded) => {
+      if (!err && decoded) {
+        req.user = decoded;
+      }
+      next();
+    });
+  } else {
+    next();
+  }
 }
-
