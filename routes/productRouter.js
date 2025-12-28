33e146e95f075b5f71f5ed14303d1df0ee8799ba
tiutionmanager-max <harithaweerasekara128@gmail.com>
2025-12-28 09:44:53 +0530

    Jwt add routes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct } from '../controllers/productController.js';
+import verifyJWT from '../middleware/auth.js';
+
+
 
 const productRouter = express.Router();
 
@@ -8,7 +11,7 @@ productRouter.get("/",getProducts)
 productRouter.get("/search/:id", searchProducts)
 productRouter.get("/:id", getProductById);
 productRouter.delete("/:productId",deleteProduct)
-productRouter.put("/:productId",updateProduct)
+productRouter.put("/:productId", verifyJWT, updateProduct);
 
 
 
