fb2f88e5d162f900075b63b2dc1e92890b73a758
tiutionmanager-max <harithaweerasekara128@gmail.com>
2025-12-29 20:16:50 +0530

    cors aded

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ mongoose.connect(process.env.MONGO_URL).then(
 
 app.use(bodyparser.json())
 
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: [
+      "https://cbc-frontend-seven.vercel.app",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter)
